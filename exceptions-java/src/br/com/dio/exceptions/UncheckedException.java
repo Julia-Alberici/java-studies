06a6e387b334abb46833af0d3465aaecb48188b9
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-15 01:22:00 -0300

    Add exceptions directory

---
+++ exceptions-java/src/br/com/dio/exceptions/UncheckedException.java
@@ -0,0 +1,33 @@
+package br.com.dio.exceptions;
+
+import javax.swing.*;
+
+// Fazer a divisão de 2 valores inteiros
+public class UncheckedException {
+    public static void main(String[] args) {
+
+        boolean continueLooping = true;
+
+        do{
+        String a = JOptionPane.showInputDialog("Numerador: ");
+        String b = JOptionPane.showInputDialog("Denominador: ");
+
+        try{
+            double resultado = dividir(Double.parseDouble(a), Double.parseDouble(b));
+            System.out.println("Resultado: " + resultado);
+            continueLooping  = false;
+        } catch (NumberFormatException e) {
+//            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro ");
+        } catch(ArithmeticException e){
+            JOptionPane.showMessageDialog(null, "Impossível dividir um número por zero");
+        } finally{
+            System.out.println("Chegou no finally!");
+        }
+        }while(continueLooping);
+
+        System.out.println("O código continua...");
+    }
+
+    public static double dividir(double a, double b) {return a/b;}
+}
